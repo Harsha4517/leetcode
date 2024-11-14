634f43f1000cf9c7310f84e5527cac350d12531d
Harsha4517 <anumolusriharsha45@gmail.com>
2024-11-14 21:23:19 +0530

    feat: p_13

---
+++ p_13/p_13.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Leetcode url: https://leetcode.com/problems/roman-to-integer/description/
+func main() {
+	res := romanToInt("LVIII")
+	fmt.Println(res)
+}
+
+func romanToInt(s string) int {
+	sum := 0
+
+	rim := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+
+	for i, v := range s {
+		sum += rim[string(v)]
+		if i != 0 {
+			if rim[string(s[i-1])] < rim[string(v)] {
+				sum -= 2 * rim[string(s[i-1])]
+			}
+		}
+	}
+	return sum
+}
